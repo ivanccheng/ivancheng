a5eca43c9bc4dedcc365f00be1c4ceb30cd93a0f
ivanccheng <chengivanc@gmail.com>
2023-05-05 16:11:40 -0400

    hash route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from './components/Header'
 import About from './components/About'
